f8cd74a4ca82bf8a6fa750114f783f3daa7c20ba
ert-jill <57490736+ert-jill@users.noreply.github.com>
2020-03-18 11:07:51 +0800

    Add files via upload
    ROUND ROBIN SCHEDULING

---
+++ RoundRobinScheduling_BENATIRO/app/app/src/main/java/Scheduling/Main.java
@@ -0,0 +1,215 @@
+
+package Scheduling;
+import java. util. Scanner;
+public class Main {
+
+    static void findWaitingTime(int processes[], int n, 
+
+                 int bt[], int wt[], int quantum) 
+
+    { 
+
+        int rem_bt[] = new int[n]; 
+
+        for (int i = 0 ; i < n ; i++) 
+
+            rem_bt[i] =  bt[i]; 
+
+       
+
+        int t = 0; 
+
+        while(true) 
+
+        { 
+
+            boolean done = true; 
+
+       
+
+            
+
+            for (int i = 0 ; i < n; i++) 
+
+            { 
+
+                
+                if (rem_bt[i] > 0) 
+
+                { 
+
+                    done = false; 
+       
+
+                    if (rem_bt[i] > quantum) 
+
+                    { 
+
+                       
+                        t += quantum; 
+
+       
+
+                       
+                        rem_bt[i] -= quantum; 
+
+                    } 
+
+       
+
+                  
+
+                    else
+
+                    { 
+
+                       
+                        t = t + rem_bt[i]; 
+
+       
+
+                     
+
+                        wt[i] = t - bt[i]; 
+
+       
+
+                       
+                        rem_bt[i] = 0; 
+
+                    } 
+
+                } 
+
+            } 
+
+       
+
+            
+
+            if (done == true) 
+
+              break; 
+
+        } 
+
+    } 
+
+       
+
+    
+    static void findTurnAroundTime(int processes[], int n, 
+
+                            int bt[], int wt[], int tat[]) 
+
+    { 
+
+        
+
+        for (int i = 0; i < n ; i++) 
+
+            tat[i] = bt[i] + wt[i]; 
+
+    } 
+
+       
+
+
+    static void findavgTime(int processes[], int n, int bt[], 
+
+                                         int quantum) 
+
+    { 
+
+        int wt[] = new int[n], tat[] = new int[n]; 
+
+        int total_wt = 0, total_tat = 0; 
+
+       
+
+
+        findWaitingTime(processes, n, bt, wt, quantum); 
+
+       
+
+        findTurnAroundTime(processes, n, bt, wt, tat); 
+
+       
+
+        
+
+        System.out.println("Processes " + " Burst time " + 
+
+                      " Waiting time " + " Turn around time"); 
+
+       
+
+       
+        for (int i=0; i<n; i++) 
+
+        { 
+
+            total_wt = total_wt + wt[i]; 
+
+            total_tat = total_tat + tat[i]; 
+
+            System.out.println("\t\t\t" + (i+1) + "\t\t\t\t\t\t" + bt[i] +"\t\t\t\t\t\t " + 
+
+                              wt[i] +"\t\t\t\t\t\t\t\t " + tat[i]); 
+
+        } 
+
+       
+
+        System.out.println("Average waiting time = " + 
+
+                          (float)total_wt / (float)n); 
+
+        System.out.println("Average turn around time = " + 
+
+                           (float)total_tat / (float)n); 
+
+    } 
+
+      
+
+    public static void main(String[] args) 
+    { 
+        int processes[] = { 1, 2, 3, 4}; 
+
+        int n = processes.length; 
+
+        int burst_time[] = {10, 5, 8,20}; 
+      
+
+        int quantum = 2; 
+
+        findavgTime(processes, n, burst_time, quantum); 
+        Scanner input =new Scanner(System. in);
+     while (0==0)
+     {
+     System. out. println("");
+     System. out. println("");
+        System. out. print("Arrival Process: ");
+        int cp=0;
+          int p[]=new int[processes.length+1];
+          for(int v:processes)
+          p[cp++]=v;
+          p[processes.length]=input.nextInt();
+          processes=p;
+          
+          System. out. print("Arrival Process Burst_Time: ");
+          int cb=0;
+          int b[] =new int[burst_time.length+1];
+          for(int v0:burst_time)
+          b[cb++]=v0;
+          b[burst_time.length]=input.nextInt();
+          burst_time=b;
+          
+          n=processes.length;
+          findavgTime(processes, n, burst_time, quantum); 
+       }
+
+    } 
+}
+
